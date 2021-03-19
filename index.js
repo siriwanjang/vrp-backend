38b467ea48907cfa4674d6c698925a6a20049ec8
volkswg <sy.siriwanjang@gmail.com>
2021-03-20 03:03:00 +0700

    api index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const db = require("./connections/database");
+const fs = require("fs");
+// const db = require("./connections/database");
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -7,10 +8,10 @@ app.use(express.json());
 const port = 4000;
 
 // test db
-db.dbConnect();
-db.query("SELECT * FROM users", (err, result, field) => {
-  console.log((err, result));
-});
+// db.dbConnect();
+// db.query("SELECT * FROM users", (err, result, field) => {
+//   console.log((err, result));
+// });
 // test db
 
 app.get("/", (req, res) => {
@@ -18,12 +19,44 @@ app.get("/", (req, res) => {
 });
 
 app.post("/api", (req, res) => {
+  let ret_data = { status: { success: false, description: "" }, data: null };
+
   const api = req.body.api;
   const method = req.body.method;
   const data = req.body.data;
 
-  console.log(api, method, data);
-  res.send("success");
+  try {
+    if (fs.existsSync(`./API/${api}.js`)) {
+      const requestAPI = require("./API/" + api);
+      // check call undefined method
+      if (typeof requestAPI[method] === "undefined") {
+        throw "call undefined method";
+      } else {
+        requestAPI[method](data, (result) => {
+          // console.log(result);
+          ret_data.status.success = result.success;
+          ret_data.status.description = result.description;
+          ret_data.data = result.data;
+          if (ret_data.data === null) {
+            delete ret_data.data;
+          }
+          res.send(ret_data);
+          return;
+        });
+      }
+    } else {
+      throw `There are no ${api} here`;
+    }
+  } catch (err) {
+    // console.log(err);
+    ret_data.status.success = false;
+    ret_data.status.description = err;
+    if (ret_data.data === null) {
+      delete ret_data.data;
+    }
+
+    res.send(ret_data);
+  }
 });
 
 app.listen(port, () => {
