95aa4ac9581ad3cd97652c15653e14f7a68142b8
volkswg <sy.siriwanjang@gmail.com>
2021-03-20 03:03:11 +0700

    utility function

---
+++ Utility/Util.js
@@ -0,0 +1,49 @@
+module.exports = {
+  isValid: (data, dataType, allowNull = false) => {
+    //   console.log("data", data);
+    if (!allowNull && (data === undefined || data === "")) {
+      return false;
+    } else {
+      switch (dataType) {
+        case "string":
+          if (typeof data === "string") {
+            return true;
+          }
+          break;
+        case "number":
+          if (!isNaN(data)) {
+            return true;
+          }
+          break;
+        default:
+          return true;
+      }
+    }
+
+    return false;
+  },
+  zeroPadding: (paddingAmount, data) => {
+    return ("0000000000" + data).substr(-paddingAmount);
+  },
+  empty: () => {},
+  getNowDate: () => {
+    const timestamp = new Date();
+    const year = timestamp.getFullYear();
+    const month = module.exports.zeroPadding(2, timestamp.getMonth() + 1);
+    const day = module.exports.zeroPadding(2, timestamp.getDate());
+
+    return `${year}-${month}-${day}`;
+  },
+  getDateTime: () => {
+    const timestamp = new Date();
+    const year = timestamp.getFullYear();
+    const month = module.exports.zeroPadding(2, timestamp.getMonth() + 1);
+    const day = module.exports.zeroPadding(2, timestamp.getDate());
+
+    const hour = module.exports.zeroPadding(2, timestamp.getHours());
+    const minute = module.exports.zeroPadding(2, timestamp.getMinutes());
+    const seccond = module.exports.zeroPadding(2, timestamp.getSeconds());
+
+    return `${year}-${month}-${day} ${hour}:${minute}:${seccond}`;
+  },
+};
