fe6e3993d2dde974f7b5dfea2daf3adbcd960653
volkswg <sy.siriwanjang@gmail.com>
2021-03-20 03:03:27 +0700

    use remote database

--- connections/database.js
+++ connections/database.js
@@ -1,11 +1,18 @@
 var mysql = require("mysql");
 
+// const localDBConfig = {
+//   connectionLimit: 10,
+//   host: process.env.DATABASE_ENDPOINT || "localhost",
+//   user: process.env.DATABASE_USER || "root",
+//   password: process.env.DATABASE_PASSWORD || "",
+//   database: "vrp_project",
+// };
 const localDBConfig = {
   connectionLimit: 10,
-  host: process.env.DATABASE_ENDPOINT || "localhost",
-  user: process.env.DATABASE_USER || "root",
-  password: process.env.DATABASE_PASSWORD || "",
-  database: "vrp_project",
+  host: "app.aws.wasin.me",
+  user: "vrpapp",
+  password: "vrpapp0000",
+  database: "app_db",
 };
 
 let conn = null;
